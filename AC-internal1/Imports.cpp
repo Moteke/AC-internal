2005ae7b0ef808af12627fa861dad12a227e3920
Piotr Politowicz <P.A.Politowicz@student.tudelft.nl>
2022-05-04 16:58:55 +0200

    Add debug messages to imports

--- AC-internal1/Imports.cpp
+++ AC-internal1/Imports.cpp
@@ -1,4 +1,5 @@
 #include "imports.h"
+#include <iostream>
 
 Imports::Imports()
 {
@@ -12,19 +13,30 @@ bool Imports::initialize()
         Need to refactor the 'return false' problems
         into proper debug console errors
     */
-    if (sdl2 == NULL) return false;
+    if (sdl2 == NULL) {
+        std::cout << "FATAL: SDL2.DLL NO HANDLE" << '\n';
+        return false;
+    }
+    
+    if (SDL_ShowCursor = (SDL_ShowCursor_t*)GetProcAddress(sdl2, "SDL_ShowCursor"), SDL_ShowCursor == NULL) {
+        std::cout << "ERROR: SDL_ShowCursor() NO HANDLE" << '\n';
+        return false;
+    }
 
-    SDL_ShowCursor = (SDL_ShowCursor_t*)GetProcAddress(sdl2, "SDL_ShowCursor");
-    if (SDL_ShowCursor == NULL) return false;
-
-    SDL_SetRelativeMouseMode = (SDL_SetRelativeMouseMode_t*)GetProcAddress(sdl2, "SDL_SetRelativeMouseMode");
-    if (SDL_SetRelativeMouseMode == NULL) return false;
-
-    SDL_SetWindowGrab = (SDL_SetWindowGrab_t*)GetProcAddress(sdl2, "SDL_SetWindowGrab");
-    if (SDL_SetWindowGrab == NULL) return false;
-
-    wglSwapBuffers = (wglSwapBuffers_t*)GetProcAddress(opengl32, "wglSwapBuffers");
-    if (wglSwapBuffers == NULL) return false;
+    if (SDL_SetRelativeMouseMode = (SDL_SetRelativeMouseMode_t*)GetProcAddress(sdl2, "SDL_SetRelativeMouseMode"), SDL_SetRelativeMouseMode == NULL) {
+        std::cout << "ERROR: SDL_SetRelativeMouseMode() NO HANDLE" << '\n';
+        return false;
+    }
+    
+    if (SDL_SetWindowGrab = (SDL_SetWindowGrab_t*)GetProcAddress(sdl2, "SDL_SetWindowGrab"), SDL_SetWindowGrab == NULL) {
+        std::cout << "ERROR: SDL_SetWindowGrab() NO HANDLE" << '\n';
+        return false;
+    }
+    
+    if (wglSwapBuffers = (wglSwapBuffers_t*)GetProcAddress(opengl32, "wglSwapBuffers"), wglSwapBuffers == NULL) {
+        std::cout << "ERROR: wglSwapBuffers() NO HANDLE" << '\n';
+        return false;
+    }
 
     return true;
 }
